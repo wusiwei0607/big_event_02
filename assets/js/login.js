62ec3cd4f8ddbbf82ded300dc20006da04683b05
wusiwei <120749792@qq.com>
2020-10-27 19:02:15 +0800

    注册和登录功能完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -26,13 +26,14 @@ $(function () {
     })
 
     // 3. 提交注册表单,发起Ajax请求
+    var layer = layui.layer
     $("#form-reg").on('submit', function (e) {
         // 阻止表单默认提交
         e.preventDefault()
         // 发起Ajax请求
         $.ajax({
             type: 'POST',
-            url: 'http://ajax.frontend.itheima.net/api/reguser',
+            url: '/api/reguser',
             data: {
                 username: $('#form-reg input[name=username]').val(),
                 password: $('#form-reg input[name=username]').val()
@@ -42,7 +43,34 @@ $(function () {
                 if (res.status !== 0) {
                     layer.msg(res.message)
                 }
-                layer.msg(res.message)
+                layer.msg('注册成功,请登录!')
+                // 跳转到登录表单
+                $('#link-login').trigger('click')
+                // 清除表单数据
+                $('#form-reg')[0].reset()
+            }
+        })
+    })
+
+    // 4. 提交登录表单,发起Ajax请求
+    $('#form-login').on('submit', function (e) {
+        // 阻止表单默认提交
+        e.preventDefault();
+        var data = $(this).serialize()
+        // 发起Ajax请求
+        $.ajax({
+            method: 'POST',
+            url: '/api/login',
+            data,
+            success: function (res) {
+                // console.log(res);
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                layer.msg('登录成功!')
+                localStorage.setItem('token', res.token)
+                // 跳转到后台首页
+                location.href = '/index.html'
             }
         })
     })
